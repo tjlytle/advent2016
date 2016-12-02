9fc75d3ac6986c6a6c2f4cd421f3a3e4d8b0058d
Tim Lytle <tim@timlytle.net>
2016-12-02 15:43:39 -0500

    Moved Day1 input out of code.

--- src/Day1/solve.php
+++ src/Day1/solve.php
@@ -2,8 +2,7 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $taxi = new \Advent2016\Day1\Taxi();
-$directions = 'R1, R1, R3, R1, R1, L2, R5, L2, R5, R1, R4, L2, R3, L3, R4, L5, R4, R4, R1, L5, L4, R5, R3, L1, R4, R3, L2, L1, R3, L4, R3, L2, R5, R190, R3, R5, L5, L1, R54, L3, L4, L1, R4, R1, R3, L1, L1, R2, L2, R2, R5, L3, R4, R76, L3, R4, R191, R5, R5, L5, L4, L5, L3, R1, R3, R2, L2, L2, L4, L5, L4, R5, R4, R4, R2, R3, R4, L3, L2, R5, R3, L2, L1, R2, L3, R2, L1, L1, R1, L3, R5, L5, L1, L2, R5, R3, L3, R3, R5, R2, R5, R5, L5, L5, R2, L3, L5, L2, L1, R2, R2, L2, R2, L3, L2, R3, L5, R4, L4, L5, R3, L4, R1, R3, R2, R4, L2, L3, R2, L5, R5, R4, L2, R4, L1, L3, L1, L3, R1, R2, R1, L5, R5, R3, L3, L3, L2, R4, R2, L5, L1, L1, L5, L4, L1, L1, R1';
-$taxi->setDirections($directions);
+$taxi->setDirections(file_get_contents(__DIR__ . '/input.txt'));
 
 echo 'Distance is: ' . $taxi->getDistance() . PHP_EOL;
 echo 'HQ is: ' . $taxi->getFirstRevisit() . PHP_EOL;
